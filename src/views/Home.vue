044dcb6ccf79ec24c170f8e529ba4e815e403144
HirokiNakashima <hiroki123naka4ma@gmail.com>
2018-10-08 10:55:54 +0900

    add extensions setting

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     <div>
       <full-page ref="fullpage" :options="options" id="fullpage">
         <div class="section">
-          <button class="next" @click="$refs.fullpage.api.moveSectionDown()">Next</button>
+          <!-- <button class="next" @click="$refs.fullpage.api.moveSectionDown()">Next</button> -->
           First section ...
         </div>
         <div class="section">
@@ -13,7 +13,7 @@
           <div class="slide"> Slide 4 </div>
         </div>
         <div class="section">
-          <button class="prev" @click="$refs.fullpage.api.moveSectionUp()">Prev</button>
+          <!-- <button class="prev" @click="$refs.fullpage.api.moveSectionUp()">Prev</button> -->
           Second section ...
         </div>
         <div class="section">
@@ -25,6 +25,10 @@
 </template>
 
 <script>
+import 'fullpage.js/dist/fullpage.extensions.min.js'
+// https://github.com/alvarotrigo/fullPage.js/wiki/How-to-activate-a-fullPage.js-extension
+// https://alvarotrigo.com/fullPage/extensions/scroll-horizontally.html#firstPage
+
 export default {
   metaInfo: {
     title: 'Fullpagetest',
@@ -34,13 +38,16 @@ export default {
   data() {
     return {
       options: {
+        licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
         menu: '#menu',
         anchors: ['page1', 'page2', 'page3', 'page4'],
         sectionsColor: ['#38caca', '#ff5f45', '#0798ec', '#9728ec'],
         controlArrows: true,
         navigation: false,
         navigationPosition: 'right',
+        autoScrolling: true,
         scrollHorizontally: true,
+        scrollHorizontallyKey: '',
         continuousHorizontal: true,
         dragAndMove: true,
       },
